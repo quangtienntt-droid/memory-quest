76957afddae84af97c3a5a448f7084c8e3ad18a5
Quang Tien <quangtienntt@gmail.com>
2026-03-08 09:09:21 +0700

    fix express route for render

--- server.ts
+++ server.ts
@@ -125,9 +125,9 @@ async function startServer() {
   } else {
     // Chế độ thực tế (Production) - Serve thư mục dist sau khi build
     app.use(express.static(path.join(__dirname, "dist")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html"));
-    });
+   app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
   }
 
   // Lắng nghe kết nối
